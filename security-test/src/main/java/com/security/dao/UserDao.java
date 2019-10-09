0152396c9aeee42572525cae08b9402888c0f0df
root <3126037048@qq.com>
2019-10-09 13:24:09 +0800

    测试完整提交

---
+++ security-test/src/main/java/com/security/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.security.dao;
+
+/**
+ * @InterfaceName UserDao
+ * @description 访问用户表数据
+ * @Author NieQiQiang
+ * @Date 2019/7/8 13:21
+ * @Version 1.0
+ */
+public interface UserDao {
+}
